efda99251f6686fd352cb90c3a92fe963f58fb95
William Hideki Nakata <whnakata@gmail.com>
2018-10-04 11:49:42 -0300

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+# Create your models here.
+from django.utils import timezone
+from django.db import models
+
+
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='Texto da Publicação')
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,6 @@
+# Create your views here
+
+from django.shortcuts import render
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
