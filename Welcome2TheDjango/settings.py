cc0e98b6b663952189c4d53d3012c2d04b9a14e3
William Hideki Nakata <whnakata@gmail.com>
2018-10-04 14:13:46 -0300

    pythonanywhere update

--- Welcome2TheDjango/settings.py
+++ Welcome2TheDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fk4ygr#8!r23rrbftx*m^scsj2)zqpua_uqxer3&k(-3ce@1!-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
